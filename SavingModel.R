5ef67226c4deaedfc8952d544cc3138c1c4aa5aa
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-06-04 13:12:22 +0300

    saved the model

--- SavingModel.R
+++ SavingModel.R
@@ -1,5 +1,5 @@
 # Saving the Linear Regression model
-saveRDS(lm_model, "./models/saved_lm_model.rds")
+saveRDS(model_cv, "./models/saved_lm_model.rds")
 
 # Load the saved linear regression model
 loaded_lm_model <- readRDS("./models/saved_lm_model.rds")
@@ -7,11 +7,11 @@ loaded_lm_model <- readRDS("./models/saved_lm_model.rds")
 # Prepare new data for prediction
 new_data <- data.frame(
   Age = as.numeric(30),
-  Gender = factor("Male"),
-  Education_Level = factor("Master's"),
-  Job_Title = character(0),
+  Gender = factor("Female"),
+  Education_Level = factor("PhD"),
+  Job_Title = "Software Engineer",
   Years_of_Experience = as.numeric(10),
-  Salary = as.numeric(0)  # Placeholder value for prediction
+  Salary = as.numeric(1000)
 )
 
 # Use the loaded model to make predictions
